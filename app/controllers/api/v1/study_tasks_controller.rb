c13fce8e6a1082c88cd96c8a1cf1a4569e658b82
Ky Kartchner <kartchnerky@gmail.com>
2021-03-26 16:34:12 -0600

    Study Task - create, read, update, delete: - Add study task controller CRUD operations - Add study_task_requests - Add json convert to switch from camelCase to snake_case
    Closes #3

---
+++ app/controllers/api/v1/study_tasks_controller.rb
@@ -0,0 +1,65 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class StudyTasksController < ApplicationController
+      def index
+        study_tasks = StudyTask.order('created_at DESC')
+        render json: { status: 'SUCCESS', message: 'Study tasks loaded', data: study_tasks },
+               status: :ok
+      end
+
+      def show
+        find_resource(params[:id]) do |study_task|
+          render json: { status: 'SUCCESS', message: 'Study task loaded', data: study_task },
+                 status: :ok
+        end
+      end
+
+      def create
+        study_task = StudyTask.new(study_task_params)
+
+        if study_task.save
+          render json: { status: 'SUCCESS', message: 'Study task created', data: study_task },
+                 status: :ok
+        else
+          render json: { status: 'ERROR', message: 'Study task not saved',
+                         data: study_task.errors }, status: :unprocessable_entity
+        end
+      end
+
+      def update
+        find_resource(params[:id]) do |study_task|
+          if study_task.update(study_task_params)
+            render json: { status: 'SUCCESS', message: 'Study task updated', data: study_task },
+                   status: :ok
+          else
+            render json: { status: 'ERROR', message: 'Study task not updated', data: study_task },
+                   status: :unprocessable_entity
+          end
+        end
+      end
+
+      def destroy
+        find_resource(params[:id]) do |study_task|
+          study_task.destroy
+          render json: { status: 'SUCCESS', message: 'Study task deleted', data: study_task },
+                 status: :ok
+        end
+      end
+
+      private
+
+      def study_task_params
+        params.permit(:start_point, :end_point, :due_date, :study_plan_id)
+      end
+
+      def find_resource(_id, &callback)
+        study_task = StudyTask.find(params[:id])
+        callback.call(study_task)
+      rescue StandardError
+        render json: { status: 'ERROR', message: 'Resource not found' }, status: :not_found
+      end
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,31 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_26_215004) do
-
+ActiveRecord::Schema.define(version: 20_210_326_215_004) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "study_plans", force: :cascade do |t|
-    t.string "title", limit: 50
-    t.integer "points"
-    t.bit "study_days_string", limit: 7
-    t.date "start_date"
-    t.date "end_date"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+  create_table 'study_plans', force: :cascade do |t|
+    t.string 'title', limit: 50
+    t.integer 'points'
+    t.bit 'study_days_string', limit: 7
+    t.date 'start_date'
+    t.date 'end_date'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
   end
 
-  create_table "study_tasks", force: :cascade do |t|
-    t.integer "start_point"
-    t.integer "end_point"
-    t.date "due_date"
-    t.bit "is_complete", limit: 1
-    t.bigint "study_plan_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["study_plan_id"], name: "index_study_tasks_on_study_plan_id"
+  create_table 'study_tasks', force: :cascade do |t|
+    t.integer 'start_point'
+    t.integer 'end_point'
+    t.date 'due_date'
+    t.bit 'is_complete', limit: 1
+    t.bigint 'study_plan_id', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['study_plan_id'], name: 'index_study_tasks_on_study_plan_id'
   end
 
-  add_foreign_key "study_tasks", "study_plans"
+  add_foreign_key 'study_tasks', 'study_plans'
 end
