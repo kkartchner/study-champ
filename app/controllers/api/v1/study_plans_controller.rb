5c4b9c1fece61dd7c9e2e2c8c9159a349139c0af
Ky Kartchner <kartchnerky@gmail.com>
2021-03-26 15:22:17 -0600

    Launch json and workspace set up things

--- app/controllers/api/v1/study_plans_controller.rb
+++ app/controllers/api/v1/study_plans_controller.rb
@@ -2,47 +2,45 @@ class Api::V1::StudyPlansController < ApplicationController
   def index
     study_plans = StudyPlan.order('created_at DESC')
     render json: { status: 'SUCCESS', message: 'Study plans loaded', data: study_plans },
-      status: :ok
+           status: :ok
   end
 
   def show
     find_resource(params[:id]) do |study_plan|
       render json: { status: 'SUCCESS', message: 'Study plan loaded', data: study_plan },
-        status: :ok
+             status: :ok
     end
   end
-  
 
   def create
     study_plan = StudyPlan.new(study_plan_params)
 
     if study_plan.save
       render json: { status: 'SUCCESS', message: 'Study plan created', data: study_plan },
-        status: :ok
+             status: :ok
     else
-      render json: { status: 'ERROR', message: 'Study plan not saved', data: study_plan },
-        status: :unprocessable_entity
+      render json: { status: 'ERROR', message: 'Study plan not saved',
+                     data: study_plan.errors }, status: :unprocessable_entity
     end
-
   end
-  
+
   def update
     find_resource(params[:id]) do |study_plan|
-      if study_plan.update_attributes(study_plan_params)
+      if study_plan.update(study_plan_params)
         render json: { status: 'SUCCESS', message: 'Study plan updated', data: study_plan },
-          status: :ok
+               status: :ok
       else
         render json: { status: 'ERROR', message: 'Study plan not updated', data: study_plan },
-          status: :unprocessable_entity
+               status: :unprocessable_entity
       end
     end
   end
-  
+
   def destroy
     find_resource(params[:id]) do |study_plan|
       study_plan.destroy
       render json: { status: 'SUCCESS', message: 'Study plan deleted', data: study_plan },
-        status: :ok
+             status: :ok
     end
   end
 
@@ -52,13 +50,10 @@ class Api::V1::StudyPlansController < ApplicationController
     params.permit(:title, :points, :study_days_string, :start_date, :end_date)
   end
 
-  def find_resource(id, &callback)
-    begin
-      study_plan = StudyPlan.find(params[:id]) 
-      callback.call(study_plan)
-    rescue
-      render json: { status: 'ERROR', message: 'Resource not found' }, status: :not_found
-    end
+  def find_resource(_id, &callback)
+    study_plan = StudyPlan.find(params[:id])
+    callback.call(study_plan)
+  rescue StandardError
+    render json: { status: 'ERROR', message: 'Resource not found' }, status: :not_found
   end
-  
 end
