7546e3e17b4ba8e6d54dd38eeca35f60a202ae89
kkartchner <46807501+kkartchner@users.noreply.github.com>
2021-03-26 15:26:35 -0600

    kkartchner/issue1 (#2)
    Add Study plan model, controller, and CRUD routes
    launch.json debugging configuration
    Closes #1

---
+++ db/migrate/20210326145617_create_study_plans.rb
@@ -0,0 +1,13 @@
+class CreateStudyPlans < ActiveRecord::Migration[6.0]
+  def change
+    create_table :study_plans do |t|
+      t.string :title, limit: 50
+      t.integer :points
+      t.column :study_days_string, 'bit(7)'
+      t.date :start_date
+      t.date :end_date
+
+      t.timestamps
+    end
+  end
+end
