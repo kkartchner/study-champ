7865989db996e58ccca787a28965ea8f7dd7e470
Ky Kartchner <kartchnerky@gmail.com>
2021-03-26 13:54:54 -0600

    Study Plan model/routes: - Add study plans controllers and CRUD routes. - Add study_plan_requests file Closes #1

---
+++ app/controllers/api/v1/study_plans_controller.rb
@@ -0,0 +1,64 @@
+class Api::V1::StudyPlansController < ApplicationController
+  def index
+    study_plans = StudyPlan.order('created_at DESC')
+    render json: { status: 'SUCCESS', message: 'Study plans loaded', data: study_plans },
+      status: :ok
+  end
+
+  def show
+    find_resource(params[:id]) do |study_plan|
+      render json: { status: 'SUCCESS', message: 'Study plan loaded', data: study_plan },
+        status: :ok
+    end
+  end
+  
+
+  def create
+    study_plan = StudyPlan.new(study_plan_params)
+
+    if study_plan.save
+      render json: { status: 'SUCCESS', message: 'Study plan created', data: study_plan },
+        status: :ok
+    else
+      render json: { status: 'ERROR', message: 'Study plan not saved', data: study_plan },
+        status: :unprocessable_entity
+    end
+
+  end
+  
+  def update
+    find_resource(params[:id]) do |study_plan|
+      if study_plan.update_attributes(study_plan_params)
+        render json: { status: 'SUCCESS', message: 'Study plan updated', data: study_plan },
+          status: :ok
+      else
+        render json: { status: 'ERROR', message: 'Study plan not updated', data: study_plan },
+          status: :unprocessable_entity
+      end
+    end
+  end
+  
+  def destroy
+    find_resource(params[:id]) do |study_plan|
+      study_plan.destroy
+      render json: { status: 'SUCCESS', message: 'Study plan deleted', data: study_plan },
+        status: :ok
+    end
+  end
+
+  private
+
+  def study_plan_params
+    params.permit(:title, :points, :study_days_string, :start_date, :end_date)
+  end
+
+  def find_resource(id, &callback)
+    begin
+      study_plan = StudyPlan.find(params[:id]) 
+      callback.call(study_plan)
+    rescue
+      render json: { status: 'ERROR', message: 'Resource not found' }, status: :not_found
+    end
+  end
+  
+end
