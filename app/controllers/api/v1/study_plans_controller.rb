2e88dc5a01f93a42df25185c3aa0528e839527c2
Ky Kartchner <kartchnerky@gmail.com>
2021-03-27 16:31:16 -0600

    Autogeneration of study and subtasks  #9: - Modify create route to auto generate study tasks - Add has_many relation to study_plan model

--- app/controllers/api/v1/study_plans_controller.rb
+++ app/controllers/api/v1/study_plans_controller.rb
@@ -20,7 +20,44 @@ module Api
         study_plan = StudyPlan.new(study_plan_params)
 
         if study_plan.save
-          render json: { status: 'SUCCESS', message: 'Study plan created', data: study_plan },
+
+          ### Autogenerate study tasks ###
+          # TODO: pull out/modify logic so it first overwrites existing tasks (in the case of
+          # the "start fresh" button)
+          if study_plan.start_date.present? && study_plan.end_date.present?
+            # TODO: make total_study_days utilize the study days string
+            total_study_days = (study_plan.end_date - study_plan.start_date).to_i
+            whole_points_per_day = study_plan.points / total_study_days
+            extra_points = study_plan.points % total_study_days
+
+            cur_point = 1
+            total_study_days.times do |day_num|
+              points_to_achieve = whole_points_per_day
+              if extra_points > 0
+                points_to_achieve += 1
+                extra_points -= 1
+              end
+
+              end_point = cur_point + points_to_achieve - 1
+              # creates a study task with current point range
+              study_plan.study_tasks.create({ start_point: cur_point,
+                                              end_point: end_point,
+                                              is_complete: 0,
+                                              due_date: study_plan.start_date + day_num })
+              # TODO: create subtasks that are tied to the study task (if applicable)
+
+              cur_point = end_point + 1
+            end
+
+          end
+
+          # TODO: incorporate a translator so api returns data in camelCase
+          render json: { status: 'SUCCESS', message: 'Study plan created',
+                         data: { **study_plan.attributes,
+                           total_study_days: total_study_days,
+                           whole_points_per_day: whole_points_per_day,
+                           extra_points: study_plan.points % total_study_days,
+                           study_tasks: study_plan.study_tasks } },
                  status: :ok
         else
           render json: { status: 'ERROR', message: 'Study plan not saved',
--- app/models/study_plan.rb
+++ app/models/study_plan.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
 
 class StudyPlan < ApplicationRecord
+  has_many :study_tasks
 end
