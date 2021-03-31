03cd6b02681c9dbbc56f35fa2ada248deb9196a5
kkartchner <46807501+kkartchner@users.noreply.github.com>
2021-03-30 20:17:06 -0600

    kkartchner/issue9 (#10)
    Autogeneration of study tasks
    - Autogenerate study tasks when study plan is created
    - Based on study days string and start/end dates
    Closes #9

--- app/controllers/api/v1/study_plans_controller.rb
+++ app/controllers/api/v1/study_plans_controller.rb
@@ -20,7 +20,14 @@ module Api
         study_plan = StudyPlan.new(study_plan_params)
 
         if study_plan.save
-          render json: { status: 'SUCCESS', message: 'Study plan created', data: study_plan },
+
+          # TODO: incorporate a translator so api returns data in camelCase
+          render json: { status: 'SUCCESS', message: 'Study plan created',
+                         data: { **study_plan.attributes,
+                           study_info: { total_study_days: study_plan.total_study_days,
+                                         whole_points_per_day: study_plan.whole_points_per_day,
+                                         extra_points: study_plan.extra_points },
+                           study_tasks: study_plan.study_tasks } },
                  status: :ok
         else
           render json: { status: 'ERROR', message: 'Study plan not saved',
